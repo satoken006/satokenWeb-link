8a90dd818d33270f89f72de5906e5f2e6c279a0a
satoken912 <seat.soil.74196901@gmail.com>
2018-05-14 16:33:05 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <script src="http://satoken.nkmr.io/main.js"></script>
+    <title>リンク</title>
+</head>
+<body>
+    <div class="nav fixed">
+        <ul id="menubar">
+            <li><a href="http://satoken.nkmr.io/">HOME</a></li>
+            <li><a href="http://satoken.nkmr.io/profile/">PROFILE</a></li>
+            <li><a href="http://satoken.nkmr.io/research/">RESEARCH</a></li>
+            <li><a href="http://satoken.nkmr.io/product/">PRODUCT</a></li>
+            <li><a href="http://satoken.nkmr.io/interest/">INTEREST</a></li>
+            <li><a href="http://satoken.nkmr.io/activity/">ACTIVITY</a></li>
+            <li><a href="http://satoken.nkmr.io/link/">LINK</a></li>
+        </ul>
+    </div>
+    <img id="header" src="http://satoken.nkmr.io/img/header.jpg">
+
+    <div id="content">
+        <div class="heading">リンク一覧</div>
+        <table id="linkTable" border="0">
+            <tr>
+                <td><img class="symbol" src="../img/link/mail.png"></td>
+                <td>seat.soil.74196901[at]gmail.com（[at]を@に置き換えてください）</td>
+            </tr>
+            <tr data-href="http://seat-soil-74196901.hateblo.jp">
+                <td><img class="symbol" src="../img/link/hatenablog.png"></td>
+                <td>Satoken's log</td>
+            </tr>
+            <tr data-href="https://github.com/satoken912">
+                <td><img class="symbol" src="../img/link/github.png"></td>
+                <td>@satoken912</td>
+            </tr>
+            <tr data-href="https://www.facebook.com/satoken0617">
+                <td><img class="symbol" src="../img/link/facebook.png"></td>
+                <td>satoken0617</td>
+            </tr>
+            <tr data-href="https://twitter.com/satoken_fms">
+                <td><img class="symbol" src="../img/link/twitter.png"></td>
+                <td>@satoken_fms</td>
+            </tr>
+            <tr data-href="https://www.instagram.com/satoken006/">
+                <td><img class="symbol" src="../img/link/instagram.png"></td>
+                <td>@satoken006</td>
+            </tr>
+            <tr data-href="http://nkmr-lab.org/">
+                <td><img class="symbol" src="../img/link/laboratory.png"></td>
+                <td>中村聡史研究室</td>
+            </tr>
+            <tr data-href="https://meiji-ncc.tech/">
+                <td><img class="symbol" src="../img/link/club.png"></td>
+                <td>Nakano Computer Club（OB）</td>
+            </tr>
+        </table>
+
+    </div>
+</body>
+</html>
